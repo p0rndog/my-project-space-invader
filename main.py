eaa6a55a326f31d78e60ae56f43f9f4bf7665149
Piper <68959842+p0rndog@users.noreply.github.com>
2020-09-03 10:24:26 -0400

    you now have a tmer

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+@namespace
+class SpriteKind:
+    spacerock = SpriteKind.create()
+
+def on_on_overlap(sprite, otherSprite):
+    info.change_score_by(1)
+    otherSprite.destroy()
+    if Math.percent_chance(50):
+        sprite.say("pog", 500)
+sprites.on_overlap(SpriteKind.player, SpriteKind.spacerock, on_on_overlap)
+
+space_rocks: Sprite = None
+blackhole: Sprite = None
 effects.star_field.start_screen_effect()
-mySprite = sprites.create(img("""
+spaceship = sprites.create(img("""
         . . . . . e e e e e e . . . . . 
             . . . . e 2 2 2 2 2 2 e . . . . 
             . . . . c 2 2 2 2 2 2 c 2 . . . 
@@ -18,4 +31,58 @@ mySprite = sprites.create(img("""
             . . . 1 1 . . . . . . 1 1 . . .
     """),
     SpriteKind.player)
-    
+spaceship.bottom = scene.screen_height()
+controller.move_sprite(spaceship)
+spaceship.set_flag(SpriteFlag.STAY_IN_SCREEN, True)
+info.set_score(0)
+
+def on_update_interval():
+    global blackhole
+    blackhole = sprites.create_projectile_from_side(img("""
+            . . . . . . . . . . . . . . . . 
+                    . . . . f c f f c c c . . . . . 
+                    . . c c f f f f f f f f f . . . 
+                    . f f f f f f f f f f f f c . . 
+                    . f f f f f f f f f f f f c . . 
+                    c f f f f f f f f f f f f f c . 
+                    f f f f f f f f f f f f f f f . 
+                    c f f f f f f f f f f f f f c . 
+                    c f f f f f f f f f f f f f c . 
+                    c f f f f f f f f f f f f f f . 
+                    f f f f f f f f f f f f f f c . 
+                    c f f f f f f f f f f f f f c . 
+                    . f f f f f f f f f f f f f . . 
+                    . f f f f f f f f f f f f f . . 
+                    . . c f f f f f f f f c c . . . 
+                    . . . . f f f c c f f . . . . .
+        """),
+        0,
+        50)
+    blackhole.left = randint(0, scene.screen_width() - space_rocks.width)
+game.on_update_interval(2000, on_update_interval)
+
+def on_update_interval2():
+    global space_rocks
+    space_rocks = sprites.create_projectile_from_side(img("""
+            . . . . . . c c c . . . . . . . 
+                    . . . . . a a a c c c . . . . . 
+                    . . . c a c f a . a a c . . . . 
+                    . . c a c f f f a f f a c . . . 
+                    . c c a c c f a a c f f a c . . 
+                    . a b a a c . a a c c f a c c c 
+                    . a b b b . a b b a a c a f . c 
+                    . . a b b a f f b b a a c f f c 
+                    c . a a a c c f c b a a c f a c 
+                    c c . a a c c a a a b b a c a c 
+                    a . a b b a a . a b b . b b c . 
+                    b a c b b b . b c . c c a c . . 
+                    b a c c a b b a c . . . . . . . 
+                    b b a c a b a a . . . . . . . . 
+                    a b . b b a c . . . . . . . . . 
+                    . a a b c . . . . . . . . . . .
+        """),
+        0,
+        100)
+    space_rocks.left = randint(0, scene.screen_width() - space_rocks.width)
+    space_rocks.set_kind(SpriteKind.spacerock)
+game.on_update_interval(1000, on_update_interval2)
--- main.ts
+++ main.ts
@@ -1,5 +1,29 @@
-let space_rocks: Sprite = null
+namespace SpriteKind {
+    export const spacerock = SpriteKind.create()
+}
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
+    game.over(false)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.spacerock, function (sprite, otherSprite) {
+    otherSprite.destroy()
+    info.changeScoreBy(1)
+    if (Math.percentChance(50)) {
+        sprite.say("pog", 500)
+    }
+})
+function addScore () {
+    space_rock_spawn_time = Math.max(1000, space_rock_spawn_time - 50)
+    info.changeScoreBy(1)
+    if (info.score() % 10 == 10) {
+        info.startCountdown(countdown_time)
+    }
+}
+let blackhole: Sprite = null
+let survivor: Sprite = null
+let space_rock_spawn_time = 0
+let countdown_time = 0
 effects.starField.startScreenEffect()
+countdown_time = 20
 let spaceship = sprites.create(img`
     . . . . . e e e e e e . . . . . 
     . . . . e 2 2 2 2 2 2 e . . . . 
@@ -21,24 +45,52 @@ let spaceship = sprites.create(img`
 spaceship.bottom = scene.screenHeight()
 controller.moveSprite(spaceship)
 spaceship.setFlag(SpriteFlag.StayInScreen, true)
+spaceship.z = 10
+info.setScore(0)
+info.startCountdown(countdown_time)
+space_rock_spawn_time = 2000
 game.onUpdateInterval(1000, function () {
-    space_rocks = sprites.createProjectileFromSide(img`
-        . . . . . . c c c . . . . . . . 
-        . . . . . a a a c c c . . . . . 
-        . . . c a c f a . a a c . . . . 
-        . . c a c f f f a f f a c . . . 
-        . c c a c c f a a c f f a c . . 
-        . a b a a c . a a c c f a c c c 
-        . a b b b . a b b a a c a f . c 
-        . . a b b a f f b b a a c f f c 
-        c . a a a c c f c b a a c f a c 
-        c c . a a c c a a a b b a c a c 
-        a . a b b a a . a b b . b b c . 
-        b a c b b b . b c . c c a c . . 
-        b a c c a b b a c . . . . . . . 
-        b b a c a b a a . . . . . . . . 
-        a b . b b a c . . . . . . . . . 
-        . a a b c . . . . . . . . . . . 
+    survivor = sprites.createProjectileFromSide(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . 1 1 1 1 1 . . . . . . 
+        . . . . . 1 6 f 6 1 . . . . . . 
+        . . . . . 1 f f f 1 . . . . . . 
+        . . . . . 1 f f f 1 . . . . . . 
+        . . . . . 1 1 1 1 1 . . . . . . 
+        . . . . . . . 1 . . . . . . . . 
+        . . . . . . 1 1 1 . . . . . . . 
+        . . . . . 1 . 1 . 1 . . . . . . 
+        . . . . . 1 . 1 . 1 . . . . . . 
+        . . . . . 1 . 1 . 1 . . . . . . 
+        . . . . . . . 1 . . . . . . . . 
+        . . . . . . 1 . 1 . . . . . . . 
+        . . . . . 1 . . . 1 . . . . . . 
+        . . . . . 1 . . . 1 . . . . . . 
+        . . . . . 1 . . . 1 . . . . . . 
         `, 0, 100)
-    space_rocks.left = randint(0, scene.screenWidth() - space_rocks.width)
+    survivor.left = randint(0, scene.screenWidth() - survivor.width)
+    survivor.setKind(SpriteKind.spacerock)
+})
+forever(function () {
+    pause(space_rock_spawn_time)
+    blackhole = sprites.createProjectileFromSide(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . f c f f c c c . . . . . 
+        . . c c f f f f f f f f f . . . 
+        . f f f f f f f f f f f f c . . 
+        . f f f f f f f f f f f f c . . 
+        c f f f f f f f f f f f f f c . 
+        f f f f f f f f f f f f f f f . 
+        c f f f f f f f f f f f f f c . 
+        c f f f f f f f f f f f f f c . 
+        c f f f f f f f f f f f f f f . 
+        f f f f f f f f f f f f f f c . 
+        c f f f f f f f f f f f f f c . 
+        . f f f f f f f f f f f f f . . 
+        . f f f f f f f f f f f f f . . 
+        . . c f f f f f f f f c c . . . 
+        . . . . f f f c c f f . . . . . 
+        `, 0, 50)
+    blackhole.left = randint(0, scene.screenWidth() - blackhole.width)
+    survivor.setKind(SpriteKind.spacerock)
 })
